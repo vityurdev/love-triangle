bef830d537ffa43ca73290d980c8eb1267f28534
Vit12345 <Kennie@tut.by>
2018-02-21 23:52:14 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let numOfTriangles = 0;
+
+  for (let i = 0; i < preferences.length - 2; i++) { 
+    for (let j = i+1; j < preferences.length - 1; j++) {
+      for (let k = j+1; k < preferences.length; k++) {
+        let triangleOneExists = +((preferences[i] == j + 1) && (preferences[j] == k + 1) && (preferences[k] == i + 1));
+        let triangleTwoExists = +((preferences[i] == k + 1) && (preferences[k] == j + 1) && (preferences[j] == i + 1));
+
+        numOfTriangles += (triangleOneExists + triangleTwoExists);
+      }
+    }
+  }
+
+  return numOfTriangles;
 };
